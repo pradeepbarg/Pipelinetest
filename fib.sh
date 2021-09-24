fa42a0d83cb47037c749e5eb3d72532fb946b33a
pradeepbarg <90967047+pradeepbarg@users.noreply.github.com>
2021-09-24 23:00:04 +0530

    Update fib.sh

--- fib.sh
+++ fib.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+N=9
+
+# First Number of the
+# Fibonacci Series
+a=0
+
+# Second Number of the
+# Fibonacci Series
+b=1
+
+echo "The Fibonacci series is : "
+
+for (( i=0; i<N; i++ ))
+do
+    echo -n "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
 
